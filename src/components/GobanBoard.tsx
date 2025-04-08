93fbae49e5eaed458e3d8fde1902a5bfcf9c6fc1
Owen Liversidge <owenliversidge@Owens-MacBook-Pro-2.local>
2025-04-08 10:36:55 +0100

    add pseudocode to GobanBoard

--- src/components/GobanBoard.tsx
+++ src/components/GobanBoard.tsx
@@ -1,4 +1,10 @@
 const GobanBoard = () => {
+  // TODO:
+  // - Render a 3x3 grid representing the goban
+  // - Allow clicking on a cell to select it
+  // - Highlight the selected cell
+  // - Show whether the selected stone is captured (hardcoded at first)
+
   return <div>Goban Board</div>
 }
 
